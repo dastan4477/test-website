77280f8834f4d064ce09bb3688fc3fa318bdcee6
dastan4477 <58164311+dastan4477@users.noreply.github.com>
2023-05-17 01:03:38 +0200

    Creating carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,9 +1,9 @@
 import CarouselItem from "./CarouselItem";
-import {useState} from "react";
-import "./Carousel.css"
+import { useState } from "react";
+import "./Carousel.css";
 
 const Carousel = () => {
-    const [currentIndex, setCurrentIndex] = useState(0)
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const items = [
     {
@@ -26,12 +26,58 @@ const Carousel = () => {
     },
   ];
 
+  const updateIndex = (newIndex) => {
+    if (newIndex < 0) {
+      newIndex = 0;
+    } else if (newIndex >= items.length) {
+      newIndex = items.length - 1;
+    }
+
+    setCurrentIndex(newIndex);
+  };
+
   return (
     <div className="carousel">
-      <div className="inner"
-      style={{ transform: `translate(-${currentIndex * 100}%)`}}>
+      <div
+        className="inner"
+        style={{ transform: `translate(-${currentIndex * 100}%)` }}
+      >
         {items.map((item) => {
-          return <CarouselItem item={item} />;
+          return <CarouselItem item={item} width={"100%"} />;
+        })}
+      </div>
+      <div className="arrow-buttons">
+        <button
+          onClick={() => {
+            updateIndex(currentIndex - 1);
+          }}
+          className="arrow-button"
+        >
+          <span class="material-symbols-outlined">arrow_back_ios</span>
+        </button>
+        <button
+          onClick={() => {
+            updateIndex(currentIndex + 1);
+          }}
+          className="arrow-button"
+        >
+          <span class="material-symbols-outlined">arrow_forward_ios</span>
+        </button>
+      </div>
+      <div className="indicators">
+        {items.map((item, index) => {
+          return (
+            <button
+              onClick={() => {
+                updateIndex(index);
+              }}
+              className="indicator-button"
+            >
+              <span class={`material-symbols-outlined `}>
+                radio_button_checked
+              </span>
+            </button>
+          );
         })}
       </div>
     </div>
